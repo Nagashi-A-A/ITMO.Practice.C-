1b9eef2c3a328b7ef8a37e060ca09c589635a25a
Anton Yaroshchuk <61189090+Nagashi-A-A@users.noreply.github.com>
2020-12-26 13:13:33 +0300

    Update Triangle.h

--- ITMO.Cpp.Yaroshchuk.Ex10/Triangle.h
+++ ITMO.Cpp.Yaroshchuk.Ex10/Triangle.h
@@ -10,27 +10,60 @@ class Triangle
 public:
 		Triangle(Dot &a, Dot &b, Dot &c):A(a), B(b), C(c)
 		{		}
+		//Для композиции с копированием значений.
+		/*Triangle(Dot& a, Dot& b, Dot &c)
+		{
+			A = new Dot(a);
+			B = new Dot(b);
+			C = new Dot(c);
+		}*/
 
-		/*Triangle(Dot *a, Dot *b, Dot *c):A(a), B(b), C(c)
-		{}*/
+		/*double mathPerimeter()
+		{
+			double perimeter = (A.distanceTo(B) + B.distanceTo(C) + C.distanceTo(A)) / 2;
+			return perimeter;
+		}*/
+
+		/*void mathTriangleArea()
+		{
+			double perimeter = mathPerimeter();
+			double area = sqrt(perimeter * (perimeter - A.distanceTo(B)) * (perimeter - B.distanceTo(C)) * (perimeter - C.distanceTo(A)));
+			cout << "Area of this triangle is: " << area << endl;
+		}*/
+	
+		/*Triangle createT(Dot d1, Dot d2, Dot d3) //Композиция.
+		{
+			this->A = d1;
+			this->B = d2;
+			this->C = d3;
+		}*/
+	
+		//Для агрегации с использованием указателей
+	
+	Triangle(Dot *a, Dot *b, Dot *c):A(a), B(b), C(c)
+		{}
 
 		double mathPerimeter()
 		{
-			double perimeter = (A.distanceTo(B) + B.distanceTo(C) + C.distanceTo(A)) / 2;
+			double perimeter = (A->distanceTo(*B) + B->distanceTo(*C) + C->distanceTo(*A)) / 2;
 			return perimeter;
 		}
 
 		void mathTriangleArea()
 		{
 			double perimeter = mathPerimeter();
-			double area = sqrt(perimeter * (perimeter - A.distanceTo(B)) * (perimeter - B.distanceTo(C)) * (perimeter - C.distanceTo(A)));
+			double area = sqrt(perimeter * (perimeter - A->distanceTo(*B)) * (perimeter - B->distanceTo(*C)) * (perimeter - C->distanceTo(*A)));
 			cout << "Area of this triangle is: " << area << endl;
 		}
 
 	private:
-		/*Dot* A;
+		Dot* A;
 		Dot* B;
-		Dot* C;*/
+		Dot* C;
+	
+		/*Dot& A;
+		Dot& B;
+		Dot& C;*/
 
 		Dot A;
 		Dot B;
