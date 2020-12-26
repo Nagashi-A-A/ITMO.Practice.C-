007287718d57d811883c8ee65ef2a31777c4e260
Anton Yaroshchuk <61189090+Nagashi-A-A@users.noreply.github.com>
2020-12-26 10:36:49 +0300

    Update Triangle.h
    Sides deleted. Constructor now has variant with reference and pointers(commented).

--- ITMO.Cpp.Yaroshchuk.Ex10/Triangle.h
+++ ITMO.Cpp.Yaroshchuk.Ex10/Triangle.h
@@ -7,23 +7,33 @@ using namespace std;
 
 class Triangle
 {
-	public:
+public:
+		Triangle(Dot &a, Dot &b, Dot &c):A(a), B(b), C(c)
+		{		}
+
+		/*Triangle(Dot *a, Dot *b, Dot *c):A(a), B(b), C(c)
+		{}*/
+
+		double mathPerimeter()
+		{
+			double perimeter = (A.distanceTo(B) + B.distanceTo(C) + C.distanceTo(A)) / 2;
+			return perimeter;
+		}
+
+		void mathTriangleArea()
+		{
+			double perimeter = mathPerimeter();
+			double area = sqrt(perimeter * (perimeter - A.distanceTo(B)) * (perimeter - B.distanceTo(C)) * (perimeter - C.distanceTo(A)));
+			cout << "Area of this triangle is: " << area << endl;
+		}
+
+	private:
+		/*Dot* A;
+		Dot* B;
+		Dot* C;*/
+
 		Dot A;
 		Dot B;
 		Dot C;
-		double sideA;
-		double sideB;
-		double sideC;
-
-		Triangle(Dot &a, Dot &b, Dot &c);
-		Triangle(double x1, double y1, double x2, double y2, double x3, double y3);
-
-		void mathTriangleArea();
-		double mathPerimeter();
-		void showSidesLength();
-
-	/*private:
-		double sideA;
-		double sideB;
-		double sideC;*/
 };
+
